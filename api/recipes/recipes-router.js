ad5ba587bcd08f65fcbba7d8e30b658be04e3c67
Calvin K <calvinknaub@gmail.com>
2023-12-23 15:17:42 -0800

    built knexfile

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -9,8 +9,7 @@ router.get("/:recipe_id", (req, res, next) => {
     .catch(next);
 });
 
-router.use((err, req, res, next) => {
-  //eslint-disable-line
+router.use((err, req, res, next) => { //eslint-disable-line
   res.status(500).json({
     customMessage: "something went wrong inside the recipes router",
     message: err.message,
